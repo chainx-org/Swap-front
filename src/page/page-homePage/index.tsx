02ba0d777824c723aa5c395a9cdd0e24bee8a001
吴wbh1328551759 <1328551759@qq.com>
2021-05-11 18:12:43 +0800

    feat: add click to reverse currency function

--- src/page/page-homePage/index.tsx
+++ src/page/page-homePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header";
 import SideBar from "../../components/SideBar";
 import styled from "styled-components";
@@ -8,6 +8,7 @@ import BottomItem from "./components/CardBottom";
 import { ReactComponent as DogIcon } from "../../assets/symbols_DOGE.svg";
 import { ReactComponent as BtcIcon } from "../../assets/symbols_BTC.svg";
 import { ReactComponent as ExchangeIcon } from "../../assets/icon_exchange.svg";
+
 const Content = styled.main`
   display: flex;
   padding: 30px 40px;
@@ -27,21 +28,28 @@ const ExchangeIconStyle = styled.div`
       width: 26px;
       height: 26px;
       margin-left: 120px;
+      cursor: pointer;
     }
   }
 `;
 
+interface CoinInfo{
+  coinName: string;
+  coinIcon: React.ReactNode;
+}
+
 const HomePage = (): React.ReactElement => {
-  const a = ["XDOGE", "XBTC"];
-  const [currencyName, setCurrencyName] = useState(a);
-  const [currencyICon, setcurrencyICon] = useState([
-    <DogIcon style={SvgStyle} />,
-    <BtcIcon style={SvgStyle} />,
-  ]);
-  // const [currencyValue, setcurrencyValue] = useState(["1", "2"]);
-  const currencySwap = () => {
-    setCurrencyName(currencyName.reverse());
-  };
+  const [coinInfo, setCoinInfo] = useState<CoinInfo[]>([
+    {
+      coinName: 'XDOT',
+      coinIcon:  <DogIcon style={SvgStyle}/>
+    },
+    {
+      coinName: 'XDOGE',
+      coinIcon: <BtcIcon style={SvgStyle} />
+    }
+  ])
+
 
   return (
     <>
@@ -50,20 +58,20 @@ const HomePage = (): React.ReactElement => {
         <SideBar />
         <ContainerCard value="true" label="Swap">
           {/* 货币一 */}
-          <CardItem currencyTitle="From" currencyName={currencyName[0]}>
-            {currencyICon[0]}
+          <CardItem currencyTitle="From" currencyName={coinInfo[0].coinName}>
+            {coinInfo[0].coinIcon}
           </CardItem>
           {/* 转换icon */}
           <ExchangeIconStyle>
-            <div className="box" onClick={() => currencySwap()}>
+            <div className="box" onClick={() => setCoinInfo([...coinInfo].reverse())}>
               <div className="iconBox">
                 <ExchangeIcon />
               </div>
             </div>
           </ExchangeIconStyle>
           {/* 货币二 */}
-          <CardItem currencyTitle="To" currencyName={currencyName[1]}>
-            {currencyICon[1]}
+          <CardItem currencyTitle="To" currencyName={coinInfo[1].coinName}>
+            {coinInfo[1].coinIcon}
           </CardItem>
           {/* 底部按钮 */}
           <BottomItem
