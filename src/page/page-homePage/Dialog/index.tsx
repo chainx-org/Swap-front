b13daf0ec5858660727ab3ccd8fa3808388d5cf2
xl2412 <1293358059@qq.com>
2021-05-12 18:43:55 +0800

    style: The pop-up hierarchy has been modified

--- src/page/page-homePage/Dialog/index.tsx
+++ src/page/page-homePage/Dialog/index.tsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { DivDialog } from "./style";
-function Dialog() {
+import ContainerCard from "../../../components/Card/ContainerCard";
+import DialogItem from "./DialogItem";
+function DialogCard() {
   return (
     <DivDialog>
-      <div className="mask">
-        <div className="content">123</div>
+      <div className="mask"></div>
+      <div className="content">
+        <ContainerCard exitOption={true} title="Select a token">
+          <DialogItem />
+        </ContainerCard>
       </div>
     </DivDialog>
   );
 }
 
-export default Dialog;
+export default DialogCard;
--- src/page/page-homePage/Dialog/style.tsx
+++ src/page/page-homePage/Dialog/style.tsx
@@ -7,17 +7,34 @@ export const DivDialog = styled.div`
     position: fixed;
     left: 0;
     right: 0;
+    top: 0px;
     background-color: #000;
     opacity: 0.4;
     color: #f00;
+    z-index: 1;
   }
   .content {
     position: fixed;
-    height: 300px;
-    width: 300px;
-    left: 50%;
-    top: 50%;
+    // height: 356px;
+    width: 379px;
+    left: 523px;
+    top: 88px;
+    opacity: 1;
     background-color: #fff;
-    transform: translate(-50%, -50%);
+    background: rgba(255, 255, 255, 0.9);
+    border: 1px solid #efefef;
+    border-radius: 24px 24px 16px 16px;
+    box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.08);
+    z-index: 2;
+  }
+`;
+
+export const DialogItemList = styled.div`
+  .itemBox {
+    width: 356px;
+    height: 64px;
+    opacity: 0;
+    background: #f7f7f7;
+    border: 1px solid black;
   }
 `;
