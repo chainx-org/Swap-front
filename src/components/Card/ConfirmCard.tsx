d7d0c5b289ffbddfa9fe198204dd31071ecc69c9
xl2412 <1293358059@qq.com>
2021-05-12 18:42:52 +0800

    refactor: Extract card component

--- src/components/Card/ConfirmCard.tsx
+++ src/components/Card/ConfirmCard.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ContainerCard from '../../page/page-homePage/components/ContainerCard';
-import ETHSymbol from '../../assets/symbols_ETH.svg';
-import DOGESymbol from '../../assets/symbols_DOGE.svg';
-import ArrowBlack from '../../assets/arrow_black.svg'
-import styled from 'styled-components';
-import NormalButton from '../Button';
+import React from "react";
+// import ContainerCard from '../../page/page-homePage/components/ContainerCard';
+import ContainerCard from "./ContainerCard";
+import ETHSymbol from "../../assets/symbols_ETH.svg";
+import DOGESymbol from "../../assets/symbols_DOGE.svg";
+import ArrowBlack from "../../assets/arrow_black.svg";
+import styled from "styled-components";
+import NormalButton from "../Button";
 
 const CoinInfoWrapper = styled.div`
   position: relative;
-  .arrowDown{
+  .arrowDown {
     position: absolute;
     width: 16px;
     height: 16px;
@@ -16,41 +17,41 @@ const CoinInfoWrapper = styled.div`
     left: 50%;
     transform: translate(-50%, -100%);
   }
-  .numWrapper{
+  .numWrapper {
     width: 100%;
     padding: 29.5px 72px 14.5px 72px;
-    .numInfo{
-      background: rgba(255,255,255,0.90);
-      border: 1px solid #EFEFEF;
+    .numInfo {
+      background: rgba(255, 255, 255, 0.9);
+      border: 1px solid #efefef;
       border-radius: 20px;
       white-space: nowrap;
       display: flex;
       align-items: center;
       justify-content: space-between;
       width: 256px;
-  
-      .coinName{
+
+      .coinName {
         display: flex;
         align-items: center;
         padding: 10px 89px 10px 10px;
-        > img{
+        > img {
           width: 20px;
           height: 20px;
           padding-right: 6px;
         }
-        .name{
+        .name {
           font-size: 14px;
           color: #282828;
           font-weight: 500;
         }
       }
-      .num{
+      .num {
         font-size: 14px;
         color: #282828;
         font-weight: 500;
         padding: 10px 16px 10px 0;
       }
-      &:first-child{
+      &:first-child {
         margin-bottom: 48px;
       }
     }
@@ -59,30 +60,30 @@ const CoinInfoWrapper = styled.div`
 
 const PriceWrapper = styled.div`
   padding: 16px;
-  
-  .field{
+
+  .field {
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding-bottom: 12px;
-    .fieldName{
+    .fieldName {
       font-size: 12px;
-      color: #908E8E;
+      color: #908e8e;
       line-height: 17px;
     }
-    .fieldContent{
+    .fieldContent {
       font-family: PingFangSC-Medium;
       font-size: 12px;
-      color: #3F3F3F;
+      color: #3f3f3f;
       font-weight: 500;
       line-height: 17px;
     }
   }
-  
-  .confirmButton{
+
+  .confirmButton {
     margin-top: 12px;
   }
-`
+`;
 
 interface CoinNumItem {
   coinIcon: string;
@@ -94,55 +95,53 @@ const ConfirmCard = (): React.ReactElement => {
   const coinNumList: CoinNumItem[] = [
     {
       coinIcon: ETHSymbol,
-      coinName: 'XETH',
-      coinNum: 0.00245451
+      coinName: "XETH",
+      coinNum: 0.00245451,
     },
     {
       coinIcon: DOGESymbol,
-      coinName: 'XDOGE',
-      coinNum: 9999.0024
-    }
+      coinName: "XDOGE",
+      coinNum: 9999.0024,
+    },
   ];
 
   const backContent = (
     <PriceWrapper>
-      <div className='field'>
-        <span className='fieldName'>Price</span>
-        <span className='fieldContent'>15.83 XDOGE/XETH</span>
+      <div className="field">
+        <span className="fieldName">Price</span>
+        <span className="fieldContent">15.83 XDOGE/XETH</span>
       </div>
-      <div className='field'>
-        <span className='fieldName'>Minimum Received</span>
-        <span className='fieldContent'>15.83 XDOGE</span>
+      <div className="field">
+        <span className="fieldName">Minimum Received</span>
+        <span className="fieldContent">15.83 XDOGE</span>
       </div>
-      <div className='field'>
-        <span className='fieldName'>Price Impact</span>
-        <span className='fieldContent'>{'< 0.01%'}</span>
+      <div className="field">
+        <span className="fieldName">Price Impact</span>
+        <span className="fieldContent">{"< 0.01%"}</span>
       </div>
-      <div className='field'>
-        <span className='fieldName'>Liquidity Provider Fee</span>
-        <span className='fieldContent'>0.03926 XETH</span>
+      <div className="field">
+        <span className="fieldName">Liquidity Provider Fee</span>
+        <span className="fieldContent">0.03926 XETH</span>
       </div>
-      <NormalButton className='confirmButton' label='Confirm Swap'/>
+      <NormalButton className="confirmButton" label="Confirm Swap" />
     </PriceWrapper>
-  )
+  );
 
   return (
-    <ContainerCard exitOption title='Confirm Swap' backContent={backContent} >
+    <ContainerCard exitOption title="Confirm Swap" backContent={backContent}>
       <CoinInfoWrapper>
-        <div className='numWrapper'>
-          {coinNumList.map((item: CoinNumItem) =>
-            <div className='numInfo'>
-              <div className='coinName'>
-                <img src={item.coinIcon} alt=""/>
-                <div className='name'>{item.coinName}</div>
+        <div className="numWrapper">
+          {coinNumList.map((item: CoinNumItem) => (
+            <div className="numInfo">
+              <div className="coinName">
+                <img src={item.coinIcon} alt="" />
+                <div className="name">{item.coinName}</div>
               </div>
-              <div className='num'>{item.coinNum}</div>
+              <div className="num">{item.coinNum}</div>
             </div>
-          )}
+          ))}
         </div>
-        <img className='arrowDown' src={ArrowBlack} alt=""/>
-
-
+        <img className="arrowDown" src={ArrowBlack} alt="" />
       </CoinInfoWrapper>
     </ContainerCard>
   );
---
+++ src/components/Card/ContainerCard.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Card } from "antd";
+import closeBtn from "../../assets/close-pop.svg";
+import styled from "styled-components";
+const CardContainer = styled.div`
+  position: relative;
+  background: rgba(255, 255, 255, 0.9);
+  border: 1px solid #efefef;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.08);
+  border-radius: 24px 24px 16px 16px;
+  .cardHeader {
+    z-index: 999;
+    display: flex;
+    justify-content: space-between;
+    height: 47px;
+    padding-top: 16px;
+    padding-left: 16px;
+    padding-right: 16px;
+    border-bottom: 1px solid #efefef;
+    .title {
+      font-weight: 500;
+      height: 28px;
+      line-height: 28px;
+      color: #282828;
+      font-size: 16px;
+      font-family: PingFangSC-Medium;
+    }
+    .closeBtn {
+      height: 28px;
+      width: 28px;
+      color: #282828;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
+  .backContent {
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: #f4f4f5;
+    border: 1px solid #efefef;
+    border-radius: 24px 24px 16px 16px;
+    .empty {
+      height: 245px;
+    }
+  }
+`;
+interface CardItemProps {
+  children?: React.ReactNode;
+  className?: string;
+  title?: string;
+  backContent?: React.ReactNode;
+  exitOption: boolean;
+}
+
+function ContainerCard({
+  children,
+  title,
+  exitOption,
+  backContent,
+  className = "",
+}: CardItemProps): React.ReactElement<CardItemProps> {
+  return (
+    <CardContainer>
+      <div className="containerBox">
+        {title && (
+          <div className="cardHeader">
+            <div className="title">{title}</div>
+            {exitOption && (
+              <img className="closeBtn" src={closeBtn} alt="close" />
+            )}
+          </div>
+        )}
+        <div className="cardContent">{children}</div>
+        {backContent && (
+          <div className="backContent">
+            <div className="empty" />
+            {backContent}
+          </div>
+        )}
+      </div>
+    </CardContainer>
+  );
+}
+
+export default ContainerCard;
