d2a46d572131daf5b993f3a4cc5387c71ff6a45d
吴wbh1328551759 <1328551759@qq.com>
2021-05-12 15:08:47 +0800

    refactor: delete useless code

--- src/page/page-homePage/components/CardBottom.tsx
+++ src/page/page-homePage/components/CardBottom.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { Input, Button, Tooltip } from "antd";
+import { Button, Tooltip } from "antd";
+
 const Item = styled(Tooltip)`
   .title-info {
     display: flex;
     justify-content: space-between;
-    margin: 32px 0px 0 0px;
+    margin: 32px 0 0 0;
     & > span {
       display: inline-block;
       //   height: 17px;
@@ -20,7 +21,7 @@ const Item = styled(Tooltip)`
   .buttonDiv {
     width: 392px;
     height: 44px;
-    margin: 16px 0px 0 0px;
+    margin: 16px 0 0 0;
   }
 `;
 interface BottomItemProps {
@@ -30,16 +31,9 @@ interface BottomItemProps {
   label?: string;
   value?: string;
 }
-function BottomItem({
-  children,
-  className = "",
-  name,
-  label,
-  value,
-}: BottomItemProps): React.ReactElement<BottomItemProps> {
+function BottomItem({name, label, value}: BottomItemProps): React.ReactElement<BottomItemProps> {
   return (
     <Item>
-      <div></div>
       <div className="title-info">
         <span>{name}</span>
         <span>{value}</span>
