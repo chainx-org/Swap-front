39ca135beeae7be75609b49eeeb5c01598825a06
xl2412 <1293358059@qq.com>
2021-05-13 10:24:44 +0800

    style: style of the account list

--- src/components/Header/SelectAccount/AccountCard.tsx
+++ src/components/Header/SelectAccount/AccountCard.tsx
@@ -9,20 +9,31 @@ const IconStyle = {
 };
 
 function AccountCard() {
+  let address1 = "5RkTc31Cc31Cc31Cc31Cr";
   const [isAccountListOpen, setIsAccountListOpen] = useState(false);
+  const [address, setAddress] = useState(address1);
   const { accounts } = useContext(ApiContext);
   console.log("accounts", accounts);
-  function selectAccountList() {
-    isAccountListOpen === false
-      ? setIsAccountListOpen(true)
-      : setIsAccountListOpen(false);
-  }
-  const hideAllMenu = () => {
-    if (isAccountListOpen === true) {
-      setIsAccountListOpen(false);
-    }
+  const selectAccountList = (e: {
+    nativeEvent: { stopImmediatePropagation: () => void };
+  }) => {
+    e.nativeEvent.stopImmediatePropagation();
+    setIsAccountListOpen(!isAccountListOpen);
   };
+  const hideAllMenu = () => {
+    setIsAccountListOpen(false);
+  };
+
+  function addressFormate() {
+    if (address.length > 11) {
+      return setAddress(
+        address.substring(0, 5).concat("...", address.slice(-6, -1))
+      );
+    }
+    return address;
+  }
   useEffect(() => {
+    addressFormate();
     document.addEventListener("click", hideAllMenu);
   }, []);
   return (
@@ -32,7 +43,7 @@ function AccountCard() {
           <DogIcon style={IconStyle} />
         </div>
         <div className={"current-name"}>Pedro Amorim</div>
-        <div className={"current-address"}>5RkTK…c31Cr</div>
+        <div className={"current-address"}>{address}</div>
       </SelectAccountStyle>
       {isAccountListOpen === true && <SelectAccount />}
     </div>
