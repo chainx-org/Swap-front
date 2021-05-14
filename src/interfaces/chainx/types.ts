e34f868deb44a7ff9b7d0a22f51061d08fd6a510
吴wbh1328551759 <1328551759@qq.com>
2021-05-14 14:45:28 +0800

    feat: update swap types and inject Rpc when api init

--- src/interfaces/chainx/types.ts
+++ src/interfaces/chainx/types.ts
@@ -6,7 +6,7 @@ import type { ITuple } from '@polkadot/types/types';
 import type { Order } from '@polkadot/types/interfaces/parachains';
 import type { AccountId, Balance, BlockNumber, H256 } from '@polkadot/types/interfaces/runtime';
 import type { VoteWeight } from '@polkadot/types/interfaces/staking';
-import type { BtcAddress } from 'x-bridge-v2/interfaces/xGatewayBitcoinV2';
+import type { BtcAddress } from '../xGatewayBitcoinV2';
 
 /** @name AddrStr */
 export interface AddrStr extends Text {}
