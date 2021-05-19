f7c0a2a4e5392f42a5e222e62dd1a8f7abb1f551
xl2412 <1293358059@qq.com>
2021-05-19 10:31:48 +0800

    feat: add localStorage to store coinList

--- src/components/Header/SelectAccount/AccountCard.tsx
+++ src/components/Header/SelectAccount/AccountCard.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { SelectAccountStyle } from "./style";
 import SelectAccount from "./SelectAccount";
 import DogIcon from "../../../assets/symbols_DOGE.svg";
+import Wallet from "../../../assets/wallet_yellower.svg";
 import { AccountsContext } from "../../../hooks/AccountsProvider";
 import { shortenString } from "../../../helper";
 import styled from "styled-components";
@@ -14,9 +15,10 @@ const IconStyle = styled.div`
 
 function AccountCard() {
   const [isAccountListOpen, setIsAccountListOpen] = useState(false);
-  const { currentAccount } = useContext(AccountsContext);
+  const { currentAccount, isExtensionInjected } = useContext(AccountsContext);
   const [name, setName] = useState(currentAccount.name);
   const [address, setAddress] = useState(currentAccount.address);
+  const [hasAccount, setHasAccount] = useState(isExtensionInjected);
   const selectAccountList = (e: any) => {
     e.nativeEvent.stopImmediatePropagation();
     setIsAccountListOpen(!isAccountListOpen);
@@ -28,6 +30,9 @@ function AccountCard() {
   useEffect(() => {
     document.addEventListener("click", hideAllMenu);
   }, []);
+  useEffect(() => {
+    setHasAccount(isExtensionInjected);
+  }, [isExtensionInjected]);
 
   useEffect(() => {
     setAddress(shortenString(currentAccount.address));
@@ -37,14 +42,28 @@ function AccountCard() {
   return (
     <div>
       <SelectAccountStyle onClick={selectAccountList}>
-        <div className={"current-icon"}>
-          {/* < style={IconStyle} /> */}
-          <IconStyle>
-            <img src={DogIcon} alt="" />
-          </IconStyle>
-        </div>
-        <div className={"current-name"}>{name}</div>
-        <div className={"current-address"}>{address}</div>
+        {!hasAccount && (
+          <div>
+            <div className={"current-icon"}>
+              {/* < style={IconStyle} /> */}
+              <IconStyle>
+                <img src={Wallet} alt="" />
+              </IconStyle>
+            </div>
+            <div className={"current-name"}>Connect to a wallet</div>
+          </div>
+        )}
+        {hasAccount && (
+          <div className={"account-info"}>
+            <div className={"current-icon"}>
+              <IconStyle>
+                <img src={DogIcon} alt="" />
+              </IconStyle>
+            </div>
+            <div className={"current-name"}>{name}</div>
+            <div className={"current-address"}>{address}</div>
+          </div>
+        )}
       </SelectAccountStyle>
       {isAccountListOpen && <SelectAccount />}
     </div>
