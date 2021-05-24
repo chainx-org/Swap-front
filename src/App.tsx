05f1133dd6af6ef4b52db4750b3aeafe6343d13e
Bella-Liu9 <Bella_Liu20210301@163.com>
2021-05-24 11:40:41 +0800

    perf(eject pg-home & priceProvider): eject pg-home & priceProvider

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HomePage from "./page/page-homePage";
 import { AccountsProvider } from "./hooks/AccountsProvider";
 import { TokenProvider } from "./hooks/TokenProvider";
 import TransferProvider from "./hooks/TransferProvider";
+import { PriceProvider } from "./hooks/PriceProvider";
 
 const LayoutWrapper = styled.div`
   display: flex;
@@ -17,11 +18,12 @@ export const App: React.FC = () => {
     <>
       <LayoutWrapper id={"LayoutWrapper"}>
         <ApiProvider url="ws://127.0.0.1:9977">
-        {/* <ApiProvider url="ws://127.0.0.1:8087"> */}
           <AccountsProvider>
             <TokenProvider>
               <TransferProvider>
+                <PriceProvider>
               <HomePage />
+              </PriceProvider>
               </TransferProvider>
             </TokenProvider>
           </AccountsProvider>
