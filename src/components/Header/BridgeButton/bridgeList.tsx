da4e903026f34c80602c91de98377dd2ac21eff8
xl2412 <1293358059@qq.com>
2021-06-04 12:06:10 +0800

    fix: fix BridgeList url

--- src/components/Header/BridgeButton/bridgeList.tsx
+++ src/components/Header/BridgeButton/bridgeList.tsx
@@ -81,13 +81,13 @@ function BridgeList() {
       icon: sherpax,
       name: "ChainX Bridge",
       type: "BTC、DOGE",
-      url: "https://sherpaxbridge-pre.chainx.cc/#/",
+      url: "https://sherpaxbridge.chainx.cc/#/",
     },
     {
       icon: chainxbridge,
       name: "SherpaX",
       type: "BSC、ETH",
-      url: "https://chainxbridge-pre.chainx.cc/bridge",
+      url: "https://chainxbridge.chainx.cc/bridge",
     },
   ];
 
